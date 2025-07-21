1d464389684d319ff168f74dac429ae78359d7e7
ragavan-0705 <ragavankumar2005@gmail.com>
2025-07-21 03:40:19 -0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace KCT_Project.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

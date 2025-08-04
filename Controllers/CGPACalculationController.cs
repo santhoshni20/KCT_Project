7fd6725873ced723db7fe65d1a7eb7ff5c0fb9da
santhoshni20 <santhoshni0820@gmail.com>
2025-08-04 21:59:15 +0530

    CGPACalculation - code merge

---
+++ Controllers/CGPACalculationController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+namespace KCT_Project.Controllers
+{
+    public class CGPACalculationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

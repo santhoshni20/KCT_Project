ca1cffddaa558552dd8f78a7ed3583b5b19e7b76
santhoshni20 <santhoshni0820@gmail.com>
2025-08-26 10:49:06 -0700

    Removed faculty support (wrong code)

--- Interfaces/IFacultySupportRepository.cs
+++ Interfaces/IFacultySupportRepository.cs
@@ -1,12 +1,1 @@
-﻿using KSI_Project.Models;
-using KSI_Project.Models.DTOs;
-using KSI_Project.Models.Entity;
-
-namespace KSI_Project.Interfaces
-{
-    public interface IFacultySupportRepository
-    {
-        // Define contract methods, e.g.:
-        Task<IEnumerable<string>> GetFacultyDetailsAsync();
-    }
-}
+﻿

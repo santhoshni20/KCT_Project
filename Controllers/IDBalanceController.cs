701dae22d51ad0b464b3febd43dd6104d751c3ee
santhoshni20 <santhoshni0820@gmail.com>
2025-08-27 00:30:13 -0700

    ID Balance - frontend code merge

--- Controllers/IDBalanceController.cs
+++ Controllers/IDBalanceController.cs
@@ -1,6 +1,21 @@
-﻿namespace KCT_Project.Controllers
+﻿using KCT_Project.Interfaces;
+using KSI_Project.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KCT_Project.Controllers
 {
-    public interface IDBalanceController
+    public class IDBalanceController : Controller
     {
+        //private readonly IIDBalanceRepository _eventRepo;
+
+        //public IDBalanceController(IIDBalanceRepository eventRepo)
+        //{
+        //    _eventRepo = eventRepo;
+        //}
+
+        public IActionResult Index()
+        {
+            return View();
+        }
     }
 }

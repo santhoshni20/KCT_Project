4f6a0a76b789732b6b684a156780c2d6ba837c69
Pradakshina V <pradakshinav17@gmail.com>
2025-10-14 11:21:38 +0530

    Erase code

--- Models/DTOs/ApiResponseDTO.cs
+++ Models/DTOs/ApiResponseDTO.cs
@@ -1,11 +1,1 @@
-﻿namespace KSI_Project.Models.DTOs
-{
-    public class APIResponseDTO
-    {
-        public int StatusCode { get; set; }
-        public string Message { get; set; }
-        public object Data { get; set; }
-        public string ErrorDetails { get; set; }
-    }
-}
-
+﻿
--- Models/DTOs/CanteenListDto.cs
+++ Models/DTOs/CanteenListDto.cs
@@ -1,8 +1,1 @@
-﻿namespace KSI_Project.Models.DTOs
-{
-    public class CanteenListDto
-    {
-        public int CanteenID { get; set; }
-        public string CanteenName { get; set; }
-    }
-}
+﻿

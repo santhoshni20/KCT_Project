f325d7ba17eca6e4b2bc49dd3fe85d7f2ee253f2
santhoshni20 <santhoshni0820@gmail.com>
2025-10-15 15:15:09 +0530

    Placement support frontend

--- Controllers/PlacementSupportController.cs
+++ Controllers/PlacementSupportController.cs
@@ -1,9 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using KSI_Project.Models.Entity;
+﻿using KSI_Project.Interfaces;
 using KSI_Project.Models.DTOs;
-using KSI_Project.Interfaces;
+using KSI_Project.Models.Entity;
 using KSI_Project.Repositories;
+using KSI_Project.Repository.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 
 namespace KSI_Project.Controllers
 {
+    [ApiController]
+    [Route("[controller]")]
+    public class PlacementSupportController : Controller
+    {
+        private readonly IPlacementSupportRepository _placementSupportRepository;
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
 }

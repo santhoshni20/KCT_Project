1c47a849fddf0f2a55b3a6eed54e9f2e9625e98a
santhoshni20 <santhoshni0820@gmail.com>
2026-02-16 21:26:57 +0530

    settings - feature add

---
+++ Controllers/SettingsController.cs
@@ -0,0 +1,24 @@
+﻿using ksi.Interfaces;
+using ksi.Models.DTOs;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using System.IO;
+
+namespace ksi.Controllers
+{
+    public class SettingsController : Controller
+    {
+        public IActionResult Settings()
+        {
+            return View();
+        }
+        public IActionResult ActivityLogs()
+        {
+            return View();
+        }
+        public IActionResult AccessLevel()
+        {
+            return View();
+        }
+    }
+}
